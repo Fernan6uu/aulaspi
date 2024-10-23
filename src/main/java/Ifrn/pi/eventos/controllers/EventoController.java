f8b2ad86d00bdf0b7c738b8206abf70bdd8c2d23
Fernan6uu <pattocdojuninhoo@gmail.com>
2024-10-23 13:31:01 -0300

    criando a pagina de formulario para cadastro do evento

--- src/main/java/Ifrn/pi/eventos/controllers/EventoController.java
+++ src/main/java/Ifrn/pi/eventos/controllers/EventoController.java
@@ -1,5 +1,14 @@
 package Ifrn.pi.eventos.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class EventoController {
+	
+	@RequestMapping("/eventos/form")
+	public String form() {
+		return"formEvento";
+	}
 
 }
